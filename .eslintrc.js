7069101327b5f01ea6ef31c07ff7536162230f2a
Ahsan Iqbal-PC <axan.iqbal123@gmail.com>
2021-08-29 12:59:02 +0500

    update formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,31 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es2021: true,
+    node: true,
+    'react-native/react-native': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    '@react-native-community',
+  ],
   parserOptions: {
     parser: 'babel-eslint',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', 'react-native', 'prettier'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react-native/no-inline-styles': 2,
+  },
 };
