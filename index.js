4a430836fb4b82359bbfca39dec89c9e3a363fe8
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2022-03-02 11:31:48 +0500

    expo restructure

--- index.js
+++ index.js
@@ -4,11 +4,9 @@
 
 import {AppRegistry} from 'react-native';
 import App from './app/App';
-import {name as appName} from './app.json';
-import messaging from '@react-native-firebase/messaging';
 
-messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('Message handled in the background!', remoteMessage);
-});
+// messaging().setBackgroundMessageHandler(async remoteMessage => {
+//   console.log('Message handled in the background!', remoteMessage);
+// });
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('main', () => App);
