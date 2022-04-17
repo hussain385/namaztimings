1f8ac199ffbadd2a4343f80ce3cd03711e3faf13
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2022-04-18 02:45:14 +0500

    init

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,4 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = {
-  resolver: {
-    /* resolver options */
-    sourceExts: ['jsx', 'js', 'ts', 'tsx'], //add here
-  },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+module.exports = getDefaultConfig(__dirname);
